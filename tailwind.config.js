b24afd7d00b52ba21ab55f411bfe37a12f84fe72
fikri <fikri.aslam102519@students.unila.ac.id>
2022-09-28 20:10:33 +0700

    add

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    Content : [ 'index.html','../web/**/*.html'],
+    theme: {
+      screens: {
+      },
+      extend: {},
+    },
+    plugins: [],
+  }
+  
